7ae64d2dd4d3da67e9cfadc24a6269ab9e3df7f1
andresl123 <andreslopes2009@gmail.com>
2024-10-03 19:42:07 -0400

    fixing database commit

--- db.py
+++ db.py
@@ -1,29 +1,18 @@
 import sqlite3
-
-class ConnectDB:
-    def __init__(self,con):
-        self.con = con
-    def connect(self):
-        self.con = sqlite3.connect(".\database\database.db")
-    def commit(self):
-        self.con.commit()
-    def close(self):
-        self.con.close()
-
+con = sqlite3.connect(".\database\database.db")
 class Insert:
-
     def __init__(self,listings_price,listings_url,listings_image):
         self.listings_price = listings_price
         self.listings_url = listings_url
         self.listings_image = listings_image
-    
     def insert(self):
-        ConnectDB 
+        # zip function will make all the list the same size
+        # getting the values of price, url and image 
         for i, j, k in zip(self.listings_price,self.listings_url,self.listings_image):
             print(i.text,j.get('href'),j.text,k.get('src'))
-            self.cun.execute("""
-                INSERT INTO SCRAP(PRICE,URL,DESCRIPTION) VALUES
-                (?, ?, ?);""", (1,1,1))
+            con.execute("""INSERT INTO SCRAP(PRICE,URL,DESCRIPTION) VALUES (?, ?, ?);""", (i.text, j.get('href'), k.get('src')))
+        con.commit()
+        con.close()
 
 # cur.execute('''DROP TABLE SCRAP''')
 
@@ -33,9 +22,12 @@ class Insert:
 #          DESCRIPTION    VARCHAR2(500));''')
 
 class Select:
-    def select(self):
-        get_from_table = self.con.execute("SELECT PRICE,URL,DESCRIPTION from SCRAP")
+    def select():
+        con = sqlite3.connect(".\database\database.db")
+        get_from_table = con.execute("SELECT PRICE,URL,DESCRIPTION from SCRAP")
         for row in get_from_table:
             print("PRICE = ", row[0])
             print("URL = ",row[1])
             print("DESCRIPTION = ",row[2])
+        con.commit()
+        con.close
