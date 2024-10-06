d5b2afe93c68de41e97e394730a0300137b57b23
andresl123 <andreslopes2009@gmail.com>
2024-10-06 17:26:30 -0400

    adding basics classes and variables

--- db.py
+++ db.py
@@ -1,26 +1,53 @@
 import sqlite3
 con = sqlite3.connect(".\database\database.db")
 class Insert:
-    def __init__(self,listings_price,listings_url,listings_image):
+    def __init__(self,listings_price,listings_description,listings_url,listings_image,listings_date):
         self.listings_price = listings_price
+        self.listings_description = listings_description
         self.listings_url = listings_url
         self.listings_image = listings_image
+        self.listings_date = listings_date
     def insert(self):
         # zip function will make all the list the same size
         # getting the values of price, url and image 
-        for i, j, k in zip(self.listings_price,self.listings_url,self.listings_image):
-            print(i.text,j.get('href'),j.text,k.get('src'))
-            con.execute("""INSERT INTO SCRAP(PRICE,URL,DESCRIPTION) VALUES (?, ?, ?);""", (i.text, j.get('href'), k.get('src')))
+        for i, j, k, z, a in zip(self.listings_price,self.listings_description,self.listings_url,self.listings_image,self.listings_date):
+            if (i.text == 'Please Contact'):
+                continue
+            con.execute("""INSERT INTO SCRAP(PRICE,DESCRIPTION,URL,IMAGES,DATE) VALUES (?, ?, ?, ?, ?);""", (float(i.text.replace('$','').replace(',','')), j.text, k.get('href'), z.get('src'),a.text))
+        con.commit()
+        con.close()
+
+class Drop:
+    def __init__(self,table):
+        self.table = table
+    def drop(self):
+        con = sqlite3.connect(".\database\database.db")
+        con.execute("""DROP TABLE SCRAP;""")
+        con.commit()
+        con.close()
+
+class Create:
+    def __init__(self,table):
+        self.table = table
+    def create(self):
+        con = sqlite3.connect(".\database\database.db")
+        con.execute("""create table ?(
+                    PRICE number(100,2),
+                    DESCRIPTION varchar2(1000),
+                    URL varchar2(10000),
+                    IMAGES varchar2(10000));""", (self.table))
         con.commit()
         con.close()
 
 class Select:
     def select():
         con = sqlite3.connect(".\database\database.db")
-        get_from_table = con.execute("SELECT PRICE,URL,DESCRIPTION from SCRAP")
+        # Here we can use SELECT or BETWEEN statemet to find the price that the user want? 
+        get_from_table = con.execute("SELECT PRICE,DESCRIPTION,URL,IMAGES,DATE from SCRAP WHERE PRICE >= 999000")
         for row in get_from_table:
             print("PRICE = ", row[0])
-            print("URL = ",row[1])
-            print("DESCRIPTION = ",row[2])
-        con.commit()
+            print("DESCRIPTION = ",row[1])
+            print("URL = ",row[2])
+            print("IMAGES = ",row[3])
+            print("DATE = ",row[4])
         con.close
--- main_scrap.py
+++ main_scrap.py
@@ -2,6 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 from db import Insert
 from db import Select
+# from db import Drop
+# from db import Create
 
 # URL to scrape (you may need to update this)
 url = "https://www.kijiji.ca/b-house-for-sale/mississauga-peel-region/c35l1700276?sort=dateDesc&radius=5.0&address=Mississauga%2C+ON+L4Z+2Y8&ll=43.5996912%2C-79.6384557"
@@ -16,10 +18,21 @@ if response.status_code == 200:
     # Example of how to search for house listings (you'll need to inspect the site to get the right tags/classes)
     listings_price = soup.find_all("p", {"data-testid": "listing-price"})  # This might vary based on actual HTML
     listings_url = soup.find_all("a", {"data-testid":"listing-link"})
+    listings_description = soup.find_all("a", {"class":"sc-24a49435-0 eocwml"})
     listings_image = soup.find_all("img",{"data-testid":"listing-card-image"})
-    insert = Insert(listings_price,listings_url,listings_image)
-    insert.insert()
+    listings_date = soup.find("p", {"data-testid": "listing-date"})
+    # time_published = listings_date.text
+    print(listings_date)
+
+    # insert = Insert(listings_price,listings_description,listings_url,listings_image,listings_date)
+    # insert.insert()
     
-    select = Select
-    select.select()
+    # select = Select
+    # select.select()
+
+    # drop = Drop("SCRAP")
+    # drop.drop()
+
+    # create = Create("SCRAP")
+    # create.create()
 
